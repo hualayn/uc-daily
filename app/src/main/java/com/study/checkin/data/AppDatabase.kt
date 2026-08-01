5d5a8678c1897c8fc2f2a8e26fa2fd638cd391ef
hualayn <witity@hotmail.com>
2026-08-01 10:50:52 +0800

    up

--- app/src/main/java/com/study/checkin/data/AppDatabase.kt
+++ app/src/main/java/com/study/checkin/data/AppDatabase.kt
@@ -2,7 +2,7 @@ package com.study.checkin.data
 
 import android.content.Context
 import androidx.room.Room
-import androidx.room.Database
+import androidx.room.RoomDatabase
 
 @Database(entities = [CheckinEntity::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
